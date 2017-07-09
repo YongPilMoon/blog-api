8da2772bcb8bfad06e401ddd6afc520ab396a50b
YongPilMoon <m71661@gmail.com>
2017-07-09 23:43:37 +0900

    Add admin field

---
+++ migrations/20170709072438-unnamed-migration.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+      return queryInterface.addColumn('User', 'admin', {
+          type: Sequelize.BOOLEAN
+      });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
